8b5565fd9bf409ba8f549a5ee6e3476ebc49cb73
bek <isakhmet.bek@cbc-group.kz>
2022-04-18 11:03:53 +0600

    models and migrations

---
+++ riverstart/app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'is_deleted',
+        'is_published',
+    ];
+
+    public function category()
+    {
+        return $this->hasManyThrough(
+            Category::class,
+            ProductCategory::class,
+            'product_id',
+            'id',
+            'id',
+            'category_id'
+        );
+    }
+}
---
+++ riverstart/database/migrations/2022_04_18_035856_create_product_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id');
+            $table->bigInteger('category_id');
+            $table->unique(['product_id', 'category_id']);
+            $table->timestamps();
+
+            $table->foreign('product_id')->on('products')->references('id');
+            $table->foreign('category_id')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
